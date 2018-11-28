783bbf27b9a290928847b0a62402558aee4b860b
Suriel Asael <surielasaelrosasmendez@MacBook-Pro-de-Suriel.local>
2018-11-27 22:40:13 -0600

    URL of APi

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var GLOBAL = {
-	url: 'http://localhost:8000/api/',//'https://reward-referral-program.herokuapp.com/api/',//'http://localhost:8000/api/',
+	url: 'http://35.184.51.76/api/',//'https://reward-referral-program.herokuapp.com/api/',//'http://localhost:8000/api/',
 	ip: '127.0.0.1'
 }
--- src/app/services/globalsocket.ts
+++ src/app/services/globalsocket.ts
@@ -1,6 +1,6 @@
 export var GLOBALSOCKET = {
   urlproduction: 'https://reward-referral-program.herokuapp.com',//'http://localhost:8000';
   //urldevelopment: 'http://localhost:8000',//'https://reward-referral-program.herokuapp.com/api/',//'http://localhost:8000/api/',
-	urldevelopment: 'http://localhost:8000',//'https://reward-referral-program.herokuapp.com/api/',//'http://localhost:8000/api/',
+	urldevelopment: 'http://35.184.51.76',//'https://reward-referral-program.herokuapp.com/api/',//'http://localhost:8000/api/',
 	ip: '127.0.0.1'
 }
