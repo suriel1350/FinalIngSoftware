f991d0ce67111152c40089a1b4cd8f8125b30565
Suriel Asael <surielasaelrosasmendez@MacBook-Pro-de-Suriel.local>
2018-11-28 12:44:08 -0600

    Ports

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var GLOBAL = {
-	url: 'http://35.184.51.76/api/',//'https://reward-referral-program.herokuapp.com/api/',//'http://localhost:8000/api/',
+	url: 'http://35.237.20.107:8001/api/',//'https://reward-referral-program.herokuapp.com/api/',//'http://localhost:8000/api/',
 	ip: '127.0.0.1'
 }
